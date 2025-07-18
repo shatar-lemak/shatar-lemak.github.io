1a7a111d3694f9400220042c18b99fad8644b086
WaterMuffin <narlor200@gmail.com>
2025-07-18 16:52:26 +0300

    asd

--- js/menu.js
+++ js/menu.js
@@ -8,13 +8,13 @@ function MenuManage() {
         mobileMenuActivisated = true
         arrow.style.rotate = "90deg"
         topMenuButtons.style.top = "10vh"
-        content.style.filter = "blur(5px)"
+        content.style.filter = "blur(10px)"
         invisible_screen.style.display = "block"
     }
     else if (mobileMenuActivisated) {
         mobileMenuActivisated = false
         arrow.style.rotate = "0deg"
-        topMenuButtons.style.top = "-45vh"
+        topMenuButtons.style.top = "-60vh"
         content.style.filter = "none"
         invisible_screen.style.display = "none"
     }
@@ -29,9 +29,35 @@ document.addEventListener("click", function(e) {
         MenuManage()
     }
 
+    if (e.target.classList.contains("link_1")) { 
+        document.location = "/index.html"
+    }
+    else if (e.target.classList.contains("link_2")) { 
+        // document.location = "/index.html"
+    }
+    else if (e.target.classList.contains("link_3")) { 
+        document.location = "/galery.html"
+    }
+    else if (e.target.classList.contains("link_4")) { 
+        // document.location = "/index.html"
+    }
+    // console.log(e.target)
 })
-
 
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault()
+})
+
+
+document.getElementById("button_text_1").addEventListener("click", function(e) {
+    document.location = "/index.html"
+})
+document.getElementById("button_text_2").addEventListener("click", function(e) {
+    // document.location = "/galery.html"
+})
+document.getElementById("button_text_3").addEventListener("click", function(e) {
+    document.location = "/galery.html"
+})
+document.getElementById("button_text_3").addEventListener("click", function(e) {
+    // document.location = "/galery.html"
 })
